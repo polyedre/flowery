b8669375a790413c8c98fe02fe0af47e00c59aa2
polyedre <polyedre@disroot.org>
2021-05-19 20:24:42 +0200

    feat: add setup.py
    With this it is possible to install `flowery` as a package with the following
    command:
    
    ```
    sudo python setup.py install
    ```
    
    This is necessary to run the package with the command:
    
    ```
    python -m flowery
    ```

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="flowery",
+    version="0.1",
+    description="Do presentations in the terminal",
+    author="polyedre",
+    author_email="polyedre@disroot.org",
+)
